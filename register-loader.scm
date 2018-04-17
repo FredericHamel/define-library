3cac6c1e2091039de6b98400463dae64356527f7
Frederic Hamel <frederic.hamel123@gmail.com>
2018-04-17 14:32:05 -0400

    Implement loading of hosted library.

--- register-loader.scm
+++ register-loader.scm
@@ -1,17 +1,81 @@
 
+(define ##loaded-libraries '())
+(define ##sys-path (list #f "~~lib"))
+
 (namespace ("dl#"
             debug-mode
+            path->parts
             println-log
             has-prefix?))
 
-(define ##loaded-libraries '())
-(define ##sys-path (list #f "~~lib"))
+(namespace ("library#"
+            path->host-library
+            repo-parts->host-library
+
+            ;; library exception
+            library-not-found-exception
+
+            ;; host-library-type
+            make-library
+            library-host
+            library-username
+            library-reponame
+            library-treesep
+            library-tag
+            library-subdir))
+
+
 
 ;; Add .sld
 (##scheme-file-extensions-set!
  (cons '(".sld" . #f) ##scheme-file-extensions))
 
 (println (object->string ##scheme-file-extensions))
+
+(define-type host-library-type
+  constructor: make-library
+  (host library-host)
+  (username library-username)
+  (repo-name library-reponame)
+  (tree-sep library-treesep)
+  ;; Version
+  (tag library-tag)
+  (subdir library-subdir))
+
+(define (repo-parts->host-library parts)
+  (define (parse-username host rest)
+    (and (pair? rest)
+         (parse-reponame host (car rest) (cdr rest))))
+
+  (define (parse-reponame host username rest)
+    (and (pair? rest)
+         (parse-tree host username (car rest) (cdr rest))))
+
+  (define (parse-tree host username repo-name rest)
+    (and (pair? rest)
+         (parse-tag host username repo-name (car rest) (cdr rest))))
+
+  (define (parse-tag host username repo-name tree-sep rest)
+    (and (pair? rest)
+         (parse-subdir host username repo-name tree-sep (car rest) (cdr rest))))
+
+  (define (parse-subdir host username repo-name tree-sep tag subdir)
+    (make-library host username repo-name tree-sep tag subdir))
+
+  ;; parse-host
+  (and (pair? parts)
+       (parse-username (car parts) (cdr parts))))
+
+(define (path->host-library path)
+  (repo-parts->host-library
+    (path->parts path)))
+
+(define (library-not-found-exception libname)
+  (##raise-module-not-found-exception
+   ;; not sure if I should use this reference...
+   ##default-load-required-module
+   libname))
+
 (##load-required-module-set!
  (let ((old-load-required-module ##load-required-module))
    (lambda (module-ref)
@@ -68,15 +132,6 @@
                   (cons library-name-symbol ##loaded-libraries)))
              x))))
 
-
-     ;; has-prefix? as define in define-library
-     #;(define (has-prefix? str prefix)
-       (let ((str-len (string-length str))
-             (prefix-len (string-length prefix)))
-         (and (> str-len prefix-len)
-              (string=? prefix (substring str 0 prefix-len))
-              (substring str prefix-len str-len))))
-
      (cond
        ((vector? module-ref)
         (if (= (vector-length module-ref) 2)
@@ -94,46 +149,31 @@
                   (if (boolean? module-path)
                     (println-log "There is a bug: " module-name)
                     (load-library-by-name module-path)))))))
-          
+
        ((string? module-ref)
         (let ((has-repo? (or (has-prefix? module-ref "http:")
                              (has-prefix? module-ref "https:"))))
           (if has-repo?
-            ;; TODO: Handle https: local repo
-            (let* ()#;((repo-parts
-                     (call-with-input-string
-                       has-repo?
-                       (lambda (r)
-                         (read-all
-                           r (lambda (r) (read-line r #\/))))))
-                   (module (package#url-parts->module-type #f repo-parts "tree")))
-              ;;(println-log module-ref)
-              (error "[register-loder] remote loader not implemented yet")
-              (println-log
-                (##path-expand
-                 (##path-expand
-                  (package#module-name module)
-                  (package#module-version module))
-                 (##path-expand (package#module-path module) package#location)))
-              (let ((module-path
-                      (##path-expand
-                       (##path-expand
-                        (package#module-name module)
-                        (package#module-version module))
-                       (##path-expand (package#module-path module) package#location)))
-                    (module-symbol (##string->symbol module-path)))
-                (let ((x (##load module-path
-                          (lambda (script-line sc/ript-path) #f)
-                          #t
-                          #f
-                          #t)))
-                  (if (##fixnum? x)
-                    (err)
-                    (##begin
-                     (or (memq module-symbol ##loaded-libraries)
-                         (##set! ##loaded-libraries
-                          (cons module-symbol ##loaded-libraries)))
-                     x)))))
+            ;; host library
+            (let ((host-lib (path->host-library has-repo?)))
+              (println (object->string host-lib))
+              (let ((lib-path (path-expand
+                                (let ((subdir (library-subdir host-lib)))
+                                  (if (null? subdir)
+                                    (library-reponame host-lib)
+                                    (parts->path subdir "")))
+                                (path-expand
+                                  (library-tag host-lib)
+                                  (path-expand
+                                    (library-reponame host-lib)
+                                    (path-expand (library-username host-lib)
+                                                 (library-host host-lib)))))))
+                (let ((libname (module-resolve-in-path-indirect lib-path dl#library-user-location)))
+                  (if (boolean? libname)
+                    (println-log "[##load] bug")
+                    (load-library-by-name libname)))))
+
+            ;; local library
             (let ((module (##string->symbol module-ref)))
               (let* ((module-path (module-resolve module-ref))
                      (module-symbol (##string->symbol module-path)))
